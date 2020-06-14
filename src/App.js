56d16199cc237f46b734d55af5b772d021485427
brantchyoga <brantrporter@gmail.com>
2020-06-14 11:38:01 -0400

    working weather app, working on sumbit button with emptying value field

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import {fetchWeather} from './api/fetchWeather';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+    const {register, handleSubmit} = useForm();
+    const search = async (e) => {
+        console.log(e.query);
+        const data2 = await fetchWeather(e.query);
+        setWeather(data2);
+        setQuery('');
+        if(e.key === 'Enter'){
+            const data = await fetchWeather(query);
+
+            setWeather(data);
+            setQuery('');
+        }
+    }
+
+
+    return (
+        <div className="main-container">
+            <form onSubmit={handleSubmit(search)}>
+                <input type="text" placeholder="Search..." name="query" onChange={(e) => setQuery(e.target.value)} ref={register}/>
+                <input type="submit" />
+            </form>
+            <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}
+            />
+            
+            {weather.main && (
+                <div className='city'>
+                    <h2 className='city-name'> 
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className='city-temp'>
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className='info'>
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
